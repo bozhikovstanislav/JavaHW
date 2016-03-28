3630a04d0d3b6e0cb42c5ccc6f5947c701fa1183
Stanislav bozhikov <bozhikov.stanislav@gmail.com>
2016-03-28 20:09:17 +0300

    This is a Java program

---
+++ src/Teleric/JavaHW/Main.java
@@ -0,0 +1,10 @@
+package Teleric.JavaHW;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.printf("Test program");
+
+    }
+}
